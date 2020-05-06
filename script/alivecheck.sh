2c3795f30e4f3bd00ee6fc9817ee8419d17b03a3
mathiascode <mail@mathias.is>
2020-05-06 03:33:39 +0300

    Minor adjustment to alive checker

--- script/alivecheck.sh
+++ script/alivecheck.sh
@@ -13,7 +13,6 @@ while true; do
 
 	if [ "$(env printf '\xFE' | nc -w 15 play.kaboom.pw 25565 | wc -m)" -eq 0 ]; then
 		pkill -9 java
-		kill -9 $(ps -C ssh -o pid=)
 		echo $(date) >> ~/kill.log
 	fi
 done
