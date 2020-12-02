d174f7c365f2cfcf17609528262fcca1a8936bed
mathiascode <mail@mathias.is>
2020-12-02 20:47:47 +0200

    Restrict access to plugins folder

--- script/server.sh
+++ script/server.sh
@@ -13,6 +13,9 @@ cd ~/server/
 while true; do
 	# Make certain files and folders read-only
 
+	mkdir plugins/update/
+	chmod -R 500 plugins/update/
+    chmod 500 plugins/
 	chmod -R 500 plugins/bStats/
 	chmod -R 500 plugins/PluginMetrics/
 	chmod -R 500 plugins/ProtocolLib/
