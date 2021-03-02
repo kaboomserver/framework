642adc89dec7dfb32392a3023f6b72de57ca415e
Apmunute <munuteaugusto@gmail.com>
2021-03-02 11:45:19 -0300

    protocollib was removed

--- script/server.sh
+++ script/server.sh
@@ -14,7 +14,7 @@ while true; do
 	# Make certain files and folders read-only
 
 	mkdir debug/ dumps/ plugins/update/
-	chmod -R 500 debug/ dumps/ plugins/bStats/ plugins/PluginMetrics/ plugins/ProtocolLib/ plugins/update/
+	chmod -R 500 debug/ dumps/ plugins/bStats/ plugins/PluginMetrics/ plugins/update/
 	chmod 500 plugins/
 	chmod 400 bukkit.yml
 	chmod 400 commands.yml
