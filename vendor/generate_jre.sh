10fd2df3c368586bab3c01cc6d2aa8e2ae2c3fc3
kaboom <58372747+kaboombot@users.noreply.github.com>
2021-10-23 02:23:42 +0300

    Update OpenJDK to 17

--- vendor/generate_jre.sh
+++ vendor/generate_jre.sh
@@ -3,12 +3,12 @@
 # This script is used as a reference to generate a stripped-down JRE for the server
 
 rm -rf java/
-wget https://github.com/AdoptOpenJDK/openjdk16-binaries/releases/download/jdk-16.0.1%2B9/OpenJDK16U-jdk_x64_linux_hotspot_16.0.1_9.tar.gz
-tar -zxvf OpenJDK*
-rm OpenJDK*
+curl -L https://download.java.net/java/GA/jdk17.0.1/2a2082e5a09d4267845be086888add4f/12/GPL/openjdk-17.0.1_linux-x64_bin.tar.gz > openjdk.tar.gz
+tar -zxvf openjdk.tar.gz
+rm openjdk.tar.gz
 mv jdk* jdk/
 jdk/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug \
 	--exclude-files=**java_*.properties,**jrunscript,**keytool,**legal/** \
-	--add-modules java.desktop,java.logging,java.management,java.naming,java.net.http,java.scripting,java.sql,jdk.crypto.ec,jdk.unsupported,jdk.zipfs \
+	--add-modules java.desktop,java.instrument,java.logging,java.management,java.naming,java.net.http,java.scripting,java.sql,jdk.crypto.ec,jdk.security.auth,jdk.unsupported,jdk.zipfs \
 	--output java
 rm -rf jdk/
