f986f27d91d41bbf9104ac20a8779d61dc33842a
kaboom <58372747+kaboombot@users.noreply.github.com>
2022-10-16 03:28:50 +0300

    Update OpenJDK to 19

--- vendor/generate_jre.sh
+++ vendor/generate_jre.sh
@@ -3,7 +3,7 @@
 # This script is used as a reference to generate a stripped-down JRE for the server
 
 rm -rf java/
-curl -L https://download.java.net/java/GA/jdk18.0.1.1/65ae32619e2f40f3a9af3af1851d6e19/2/GPL/openjdk-18.0.1.1_linux-x64_bin.tar.gz > openjdk.tar.gz
+curl -L https://download.java.net/java/GA/jdk19/877d6127e982470ba2a7faa31cc93d04/36/GPL/openjdk-19_linux-x64_bin.tar.gz > openjdk.tar.gz
 tar -zxvf openjdk.tar.gz
 rm openjdk.tar.gz
 mv jdk* jdk/
